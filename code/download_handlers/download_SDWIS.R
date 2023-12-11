c201340033ea8b45df69d874a353fc08b601fdcd
EmmaLiTsai <emmali.tsai@gmail.com>
2023-12-11 17:15:49 -0500

    Tidy sdwis functions & adding census data

--- code/download_handlers/download_SDWIS.R
+++ code/download_handlers/download_SDWIS.R
@@ -3,53 +3,12 @@
 # Author: EmmaLi Tsai 
 # Date: 12/7/2023
 ################################################################################
-# Function downloads data from SDWIS
-# TODO: function currently only loops through one pwsid at a time and that is 
-#       SLOW- especially for TX which has 4586 pwsids 
-# TODO: function can only handle one contaminant at a time, but I could also 
-#       remove the contaminant and we'd download all of the violation data 
-#       for that pwsid and could filter down from there. 
+# Function downloads data from SDWIS: 
 ################################################################################
-download_SDWIS <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
-                           cont_code = c("1025")){
-  # grabbing starting dataframe to append to: 
-  violations_df <- data.frame()
-  for(i in 1:length(pwsids)){
-    print(paste0(pwsids[i], ": ", i, " out of ", length(pwsids)))
-    data_link_i <- paste0("https://data.epa.gov/efservice/violation/CONTAMINANT_CODE/", 
-                          cont_code, 
-                          "/pwsid/", 
-                          pwsids[i], "/ROWS/0:99999/CSV")
-    
-    # NOTE: a check is needed regarding whether there are pwsids with more than 
-    # 100,000 violations
-    # NOTE: some pwsids don't have any violations (ex. TX1010902), and will
-    # throw an error - a trycatch function is needed here to ensure that the 
-    # loop continues to run 
-    # source: https://stackoverflow.com/questions/8093914/use-trycatch-skip-to-next-value-of-loop-upon-error
-    possibleError <- tryCatch(
-      possibleError <- data.table::fread(data_link_i,
-                                        colClasses = "character"),
-      error=function(e) e
-    )
-    if(inherits(possibleError, "error")) next
-    
-    # if the pwsid has violations, then: 
-    violations_i <- data.table::fread(data_link_i,
-                                      colClasses = "character")
-    # appending to original df: 
-    violations_df <- rbind(violations_df, violations_i)
-  }
-  # lil sound when the function is complete: 
-  beepr::beep(2)
-  return(violations_df)
-}
-
-
 # It's  more efficient to pull all data from the api and then filter down - 
 # here's a possible second function: 
 # Corrected from: https://github.com/Environmental-Policy-Innovation-Center/water_team/blob/main/data/raw_data/sdwis/sdwis_violation/downloader_sdwis_violation.Rmd
-download_SDWIS_v2 <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
+download_SDWIS <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
                               cont_code = c("1025", "0999")){
   
   # Options here to handle scientific notation errors in web service requests: 
@@ -86,3 +45,43 @@ download_SDWIS_v2 <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"),
   # return: 
   return(sdwis_filt)
 }
+
+# on older verion of the functiont that looped through each pwsid which was 
+# GRUELING for a state as large as TX
+# download_SDWIS_v2 <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
+#                            cont_code = c("1025")){
+#   # grabbing starting dataframe to append to: 
+#   violations_df <- data.frame()
+#   for(i in 1:length(pwsids)){
+#     print(paste0(pwsids[i], ": ", i, " out of ", length(pwsids)))
+#     data_link_i <- paste0("https://data.epa.gov/efservice/violation/CONTAMINANT_CODE/", 
+#                           cont_code, 
+#                           "/pwsid/", 
+#                           pwsids[i], "/ROWS/0:99999/CSV")
+#     
+#     # NOTE: a check is needed regarding whether there are pwsids with more than 
+#     # 100,000 violations
+#     # NOTE: some pwsids don't have any violations (ex. TX1010902), and will
+#     # throw an error - a trycatch function is needed here to ensure that the 
+#     # loop continues to run 
+#     # source: https://stackoverflow.com/questions/8093914/use-trycatch-skip-to-next-value-of-loop-upon-error
+#     possibleError <- tryCatch(
+#       possibleError <- data.table::fread(data_link_i,
+#                                         colClasses = "character"),
+#       error=function(e) e
+#     )
+#     if(inherits(possibleError, "error")) next
+#     
+#     # if the pwsid has violations, then: 
+#     violations_i <- data.table::fread(data_link_i,
+#                                       colClasses = "character")
+#     # appending to original df: 
+#     violations_df <- rbind(violations_df, violations_i)
+#   }
+#   # lil sound when the function is complete: 
+#   beepr::beep(2)
+#   return(violations_df)
+# }
+
+
+
