c85dc44932d31578cbd2c896c20aaa1ccec17fed
EmmaLiTsai <emmali.tsai@gmail.com>
2023-12-21 13:27:21 -0500

    Function documentation to prep for package dev, SRF data merged

--- code/download_handlers/download_SDWIS.R
+++ code/download_handlers/download_SDWIS.R
@@ -3,13 +3,35 @@
 # Author: EmmaLi Tsai 
 # Date: 12/7/2023
 ################################################################################
-# Function downloads data from SDWIS: 
-################################################################################
 # It's  more efficient to pull all data from the api and then filter down - 
-# here's a possible second function: 
 # Corrected from: https://github.com/Environmental-Policy-Innovation-Center/water_team/blob/main/data/raw_data/sdwis/sdwis_violation/downloader_sdwis_violation.Rmd
+################################################################################
+# download_SDWIS(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
+#                cont_code = c("1025", "0999"))
+# 
+# Function to download SDWIS violation data for certain pwsids and contaminants. 
+#
+# Dependencies: 
+#   dplyr filter 
+#   xml2 read_xml, as_list
+#   data.table fread
+# 
+# Inputs: 
+#   pwsids:  character vector containing the pwsids of interest. Default is 
+#            set to pwsids = c("TX0610218", "TX0610220", "TX0610223") as an 
+#            example. 
+#   cont_code: Character vector containing codes of contaminants of interest. 
+#              Must be associated with a valid contaminant code recognized by 
+#              the EPA. Default is set to Chloride and Fluoride ("1025", "0999")
+# 
+# Output: data frame containing all contaminant records for the pwsid and 
+#         contaminants of interest, 
+# 
+# Example of use: download_SDWIS(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
+#                cont_code = c("1025", "0999"))
+################################################################################
 download_SDWIS <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
-                              cont_code = c("1025", "0999")){
+                           cont_code = c("1025", "0999")){
   
   # Options here to handle scientific notation errors in web service requests: 
   options("scipen"=10, "digits"=4)
@@ -20,7 +42,6 @@ download_SDWIS <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"),
   n_records_vi_xml <- xml2::read_xml(violation_url)
   n_records_vi_ls <- xml2::as_list(n_records_vi_xml)
   n_records_vi <- as.numeric(n_records_vi_ls$violationList$violation$TOTALQUERYRESULTS)
-  # n_records_vi <- as.numeric(n_records_vi_ls$violation_enf_assocList$violation_enf_assoc$TOTALQUERYRESULTS)
   # For loop requests records 100,000 to end and appends them to the first 
   # 100,000 records: 
   violations_raw <- data.frame()
@@ -45,43 +66,5 @@ download_SDWIS <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"),
   # return: 
   return(sdwis_filt)
 }
-
-# on older version of the function that looped through each pwsid which was 
-# GRUELING for a state as large as TX
-# download_SDWIS_v2 <- function(pwsids = c("TX0740039", "TX0750003", "TX0650002"), 
-#                            cont_code = c("1025")){
-#   # grabbing starting dataframe to append to: 
-#   violations_df <- data.frame()
-#   for(i in 1:length(pwsids)){
-#     print(paste0(pwsids[i], ": ", i, " out of ", length(pwsids)))
-#     data_link_i <- paste0("https://data.epa.gov/efservice/violation/CONTAMINANT_CODE/", 
-#                           cont_code, 
-#                           "/pwsid/", 
-#                           pwsids[i], "/ROWS/0:99999/CSV")
-#     
-#     # NOTE: a check is needed regarding whether there are pwsids with more than 
-#     # 100,000 violations
-#     # NOTE: some pwsids don't have any violations (ex. TX1010902), and will
-#     # throw an error - a trycatch function is needed here to ensure that the 
-#     # loop continues to run 
-#     # source: https://stackoverflow.com/questions/8093914/use-trycatch-skip-to-next-value-of-loop-upon-error
-#     possibleError <- tryCatch(
-#       possibleError <- data.table::fread(data_link_i,
-#                                         colClasses = "character"),
-#       error=function(e) e
-#     )
-#     if(inherits(possibleError, "error")) next
-#     
-#     # if the pwsid has violations, then: 
-#     violations_i <- data.table::fread(data_link_i,
-#                                       colClasses = "character")
-#     # appending to original df: 
-#     violations_df <- rbind(violations_df, violations_i)
-#   }
-#   # lil sound when the function is complete: 
-#   beepr::beep(2)
-#   return(violations_df)
-# }
-
 
 
--- code/download_handlers/download_sabcrosswalk.R
+++ code/download_handlers/download_sabcrosswalk.R
@@ -1,98 +1,79 @@
 ################################################################################
-# Downloading Drinking Water Datasets 
+# Downloading SABs & Crosswalk
 # Author: EmmaLi Tsai 
 # Date: 12/1/2023
 ################################################################################
-# Function takes a vector of state acronyms and pulls national drinking water 
-# data - data is then saved into the ./data/raw folder as a geojson file. 
+# download_sab(states = c("TX")) 
 # 
-# in general - this takes about 10 minutes to run for two states: 
+# Function to download service area boundaries for a particular state. 
+#
+# Dependencies: 
+#   dplyr filter, mutate
+#   sf st_area
+# 
+# Inputs: 
+#   states:  character vector containing the abbreviation of the states of 
+#            interest. Default is states = "TX"
+# 
+# Output: data frame containing pwsid data and population served based on 
+#         SDWIS data
+# 
+# Example of use: download_sab(states = ("TX"))
 ################################################################################
-download_sabcrosswalk <- function(states = c("TX")) {
-  # increase timeouts since some of these take a bit to download: 
-  options(timeout=200)
-  
-  # commenting this out for now since this function does not need to run on a 
-  # schedule: 
-  # # setting working directory - CRON default is home directory so needed to 
-  # # route it towards the location of my remote:
-  # setwd("/Users/emmalitsai/state-drinking-water")
-  # # printing for log troubleshooting purposes 
-  # print(Sys.time()) 
-  
-  # required libraries - will need to manage these dependencies in some way 
-  # to avoid having to load them each time the function is run (maybe an R package?)
+download_sab <- function(states = ("TX")){
   library(dplyr)
-  library(arcpullr)
-  library(sf)
-  library(readr)
-  library(magrittr)
-  
   ## Service area boundaries: ##
   sab <- sf::st_read("https://www.hydroshare.org/resource/9ebc0a0b43b843b9835830ffffdd971e/data/contents/temm.gpkg")
   # minor cleaning and subsetting based on state pwsids
-  sab_states <- sab %>% 
-    dplyr::filter(state_code %in% states) %>%
-    dplyr::mutate(area_miles = as.numeric(sf::st_area(.))) %>%
-    dplyr::mutate(area_miles = area_miles/27880000) %>%
-    dplyr::mutate(pop_density = population_served_count / area_miles)
-  print("Pulling SABs")
-  
-  ## Crosswalk: ##
-  # grabbing pwsids from dataset above for SQL query: 
-  sab_pwsids <- unique(sab_states$pwsid)
-  # querying from REST services: 
-  idString <- toString(sprintf("'%s'", sab_pwsids)) 
+  sab_states <- sab %>%
+    filter(state_code %in% states) %>%
+    mutate(area_miles = as.numeric(sf::st_area(.))) %>%
+    mutate(area_miles = area_miles/27880000) %>%
+    mutate(pop_density = population_served_count / area_miles)
+  return(sab_states)
+}
+
+################################################################################
+# download_blockcrosswalk(pwsids = c("TX0610218", "TX0610220", "TX0610223"))
+# 
+# Function to download the block parcel crosswalk from Blue Conduit - used 
+# for estimating pwsid characteristics (i.e., crosswalking census block 
+# demographics with pwsid geometries)
+#
+# Dependencies: 
+#   arcpullr get_table_layer
+#   dplyr mutate, case_when
+# 
+# Inputs: 
+#   pwsids:  character vector containing the pwsids of interest. Default is 
+#            set to pwsids = c("TX0610218", "TX0610220", "TX0610223") as an 
+#            example. 
+# 
+# Output: data frame containing the block parcel crosswalk for the pwsids 
+#         provided. This includes the census block and the weight associated 
+#         with it. 
+# 
+# Example of use: download_blockcrosswalk(pwsids = c("TX0610218", "TX0610220", "TX0610223"))
+################################################################################
+download_blockcrosswalk <- function(pwsids = c("TX0610218", "TX0610220", "TX0610223")){
+  library(dplyr)
+  # querying pwsids from REST services: 
+  idString <- toString(sprintf("'%s'", pwsids)) 
   sql_fmt <- "pwsid in (%s)"
   sql <- sprintf(sql_fmt, idString)
   pws_cross <- arcpullr::get_table_layer(url = "https://services8.arcgis.com/SVa1M0pGzvtS1NP4/ArcGIS/rest/services/Public_Water_System_Census_Block_Crosswalk/FeatureServer/0", 
                                          where = sql)
-  print("Pulling Crosswalk")
-  
-  # looping through states for merging and writing: 
-  for(i in 1:length(states)){
-    # subsetting for state data: 
-    state_i <- states[i]
-    sab_i <- subset(sab_states, state_code == state_i)
-    pws_cross_i <- subset(pws_cross, pwsid %in% unique(sab_i$pwsid))
-    
-    ## Crosswalk x service area boundaries ##
-    sab_cross <- merge(sab_i, pws_cross_i, by = "pwsid", all = TRUE)
-    sab_cross <- st_as_sf(sab_cross)
-    # saving locally: 
-    # file_name <- paste0(state_i, "-sabcrosswalk-raw")
-    # # NOTE: this should be a geojson but the file is so large that it times out 
-    # # saving it as rds for now: 
-    # write_rds(sab_cross, paste0("./data/raw/", file_name, ".rds"))
-    # sf::st_write(sab_cross, paste0("./data/raw/", file_name, ".geojson"))
-    
-    # writing to aws bucket
-    # NOTE that you need private credentials here 
-    # file_name <- paste0(state_i, "-sabcrosswalk-raw.rds")
-    # file path in aws bucket
-    # aws_filepath <- paste0("state-drinking-water/", state_i, "/raw/", file_name)
-    # writing to temporary location  
-    # readr::write_rds(data, file.path(tempdir(), file_name))
-    # storing temp file into a bucket - commented out for now until we decide 
-    # data storage stucture for this project: 
-    # put_object(
-    #   file = file.path(tempdir(), file_name),
-    #   object = aws_filepath,
-    #   bucket = "tech-team-data",
-    # )
-    
-    print(paste0("Data saved for ", state_i))
-    
-    # return this in the form of a data frame if only one argument was passed: 
-    if(length(states) == 1){
-      return(sab_cross)
-    }
-  } 
+  # fixing blocks missing a preceding zero: 
+  pws_cross <- pws_cross %>%
+    mutate(census_block = as.character(census_block),
+           census_block = case_when(
+             str_length(census_block) == 14 ~ paste0(0, census_block),
+             TRUE ~ census_block
+           ))
+  return(pws_cross)
 }
 
-
-# download_data(states = c("TX"))
-
+# IN THE FUTURE - would be cool of certain scripts could run on a schedule: 
 # SOP for automating R scripts on Macs: 
 # install.packages("cronR")
 # cronR::cron_rstudioaddin() -- use this interface to schedule batch-downloads 
@@ -103,32 +84,3 @@ download_sabcrosswalk <- function(states = c("TX")) {
 # alternative here using GitHub actions and wrapping all of this into a 
 # package: 
 # https://www.simonpcouch.com/blog/2020-12-27-r-github-actions-commit/
-
-# alternatively - we could also have these as separate functions: 
-download_sab <- function(states = ("TX")){
-  library(magrittr)
-  
-  ## Service area boundaries: ##
-  sab <- sf::st_read("https://www.hydroshare.org/resource/9ebc0a0b43b843b9835830ffffdd971e/data/contents/temm.gpkg")
-  # minor cleaning and subsetting based on state pwsids
-  sab_states <- sab %>% 
-    dplyr::filter(state_code %in% states) %>%
-    dplyr::mutate(area_miles = as.numeric(sf::st_area(.))) %>%
-    dplyr::mutate(area_miles = area_miles/27880000) %>%
-    dplyr::mutate(pop_density = population_served_count / area_miles)
-  return(sab_states)
-}
-
-download_blockcrosswalk <- function(pwsids = c("AR0000360", "TX0610218", 
-                                               "TX0610220", "TX0610223", "TX0610224")){
-  library(magrittr)
-  ## Crosswalk: ##
-  # querying from REST services: 
-  idString <- toString(sprintf("'%s'", pwsids)) 
-  sql_fmt <- "pwsid in (%s)"
-  sql <- sprintf(sql_fmt, idString)
-  pws_cross <- arcpullr::get_table_layer(url = "https://services8.arcgis.com/SVa1M0pGzvtS1NP4/ArcGIS/rest/services/Public_Water_System_Census_Block_Crosswalk/FeatureServer/0", 
-                                         where = sql)
-  return(pws_cross)
-}
-
